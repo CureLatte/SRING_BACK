5139f23d93f3e39b27268a136599058ae1027d5c
zhqmfkv <83353283+CureLatte@users.noreply.github.com>
2025-01-07 17:14:13 +0900

    [FIX] userFromDomain. usertoDomain update

--- src/user/infra/user.entity.ts
+++ src/user/infra/user.entity.ts
@@ -6,19 +6,31 @@ import TypeOrmBaseEntity from '../../common/entity/TypeOrmBaseEntity';
 export class UserEntity extends TypeOrmBaseEntity {
 	@Column()
 	name: string;
+	profile: string;
+	status: string;
 
 	toDomain(): User {
 		return new User({
 			id: this.id,
 			name: this.name,
+			status: this.status,
+			profile: this.profile,
+			createdAt: this.createdAt,
+			updatedAt: this.updatedAt,
+			deletedAt: this.deletedAt,
 		});
 	}
 
-	static fromDomain(user: User): UserEntity {
+	static fromDomain(domain: User): UserEntity {
 		const entity = new UserEntity();
 
-		entity.id = user.id;
-		entity.name = user.name;
+		entity.id = domain.id;
+		entity.name = domain.name;
+		entity.profile = domain.profile;
+		entity.status = domain.status.name;
+		entity.createdAt = domain.createdAt;
+		entity.updatedAt = domain.updatedAt;
+		entity.deletedAt = domain.deletedAt;
 
 		return entity;
 	}
